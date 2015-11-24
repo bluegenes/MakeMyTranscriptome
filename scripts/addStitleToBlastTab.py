52fd4086b0ef1ac290b393b8cd534a042826b145
NolanHartwick <nhartwic@ucsd.edu>
2015-11-24 13:18:56 -0800

    Fix mixed indents. replaced tabs with spaces

--- scripts/addStitleToBlastTab.py
+++ scripts/addStitleToBlastTab.py
@@ -12,18 +12,18 @@ blastD = {}
 with open(args.blast, 'r') as f:
     for line in f:
         line = line.rstrip().split('\t')
-	#import pdb; pdb.set_trace()
-	blastOrder.append(line[1])
-	blastD[line[1]] = line
+    #import pdb; pdb.set_trace()
+    blastOrder.append(line[1])
+    blastD[line[1]] = line
     f.close()
 
 #potentially huge file --> don't want this in memory
 with open(args.db2Name, 'r') as f:
     for line in f:
         line = line.rstrip().split('\t')
-	hitInfo = blastD.get(line[0], None)
-	if hitInfo is not None:
-	    hitInfo.extend(line[1:])
+    hitInfo = blastD.get(line[0], None)
+    if hitInfo is not None:
+        hitInfo.extend(line[1:])
     f.close()
 
 outExtendedTab = open(args.blast, 'w')
