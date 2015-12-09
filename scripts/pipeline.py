f5590495b4e1527b385e8e5204e5449dc220c69a
bluegenes <ntpierce@gmail.com>
2015-12-09 12:39:06 -0800

    adding mkdir -p style functionality for --out_dir

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -4,6 +4,7 @@ import sys
 from tasks_v2 import Supervisor, Task
 import task_functions_v2 as tf
 from assembler import gen_assembly_supervisor
+from quality import gen_quality_supervisor
 from annotater import gen_annotation_supervisor
 from expression import gen_expression_supervisor
 from itertools import chain
@@ -60,6 +61,7 @@ def full_args():
 def assembly_args():
     parser = argparse.ArgumentParser(description='Selected_tool : Assembler. Executing this tool will clean all provided reads and assemble them.')
     parser.add_argument('tool_selector',help=argparse.SUPPRESS)
+    parser.add_argument('-test',help='Use this flag to test the pipeline.',action='store_true')
     parser.add_argument('-u','--unpaired',help='A comma seperated list of unpaired fastq files.')
     parser.add_argument('-1','--fastq1',help='A comma seperated list of fastq files. Each file should be paired with the same indexed file in fastq2.')
     parser.add_argument('-2','--fastq2',help='A comma seperated list of fastq files. Each file should be paired with the same indexed file in fastq1.')
@@ -71,9 +73,9 @@ def assembly_args():
     parser.add_argument('--subsample_size',help='If greater than this number of reads (in millions) is provided, sub sample down to this number. Use 0 to signal that no subsampling should be performed. The deafult value is 50.', default=50,type=float)    
     parser.add_argument('--subsample_seed',help='A seed used to initialize the random number generator used during random sampling.')
     parser.add_argument('--truncate',help='snip reads down to this size if longer than this size. Default is no truncations.',type=int,default=-1)
-    parser.add_argument('-cegma',help='Use this flag to run cegma as part of the annotation pipeline. Cegma is an old tool for assesing the quality of assemblies. Normal behavior of the pipeline is to use busco for assesing assemblies. Using this flag will run cegma in addition to Busco.',action='store_true')
-    parser.add_argument('--busco_ref',help='Set the reference that busco will use for analysis',default='metazoa')
-    parser.add_argument('--transrate_ref',help='A reference that transrate will use to evaluate the quality of your assembly.',default='')
+    #parser.add_argument('-cegma',help='Use this flag to run cegma as part of the annotation pipeline. Cegma is an old tool for assesing the quality of assemblies. Normal behavior of the pipeline is to use busco for assesing assemblies. Using this flag will run cegma in addition to Busco.',action='store_true')
+    #parser.add_argument('--busco_ref',help='Set the reference that busco will use for analysis',default='metazoa')
+    #parser.add_argument('--transrate_ref',help='A reference that transrate will use to evaluate the quality of your assembly.',default='')
     parser.add_argument('-no_log',help='Pipeline will delete log files.',action='store_true')
     parser.add_argument('-force',help='Use this flag to perform a fresh run of the pipeline. All steps will be executed regradless of what has already been performed.',action='store_true')
     parser.add_argument('--cpu', help='Sets the process cap for execution. Default is 12. Use 0 to indicate no process cap should be used.',default=12,type=int)
@@ -82,9 +84,30 @@ def assembly_args():
     parser.add_argument('-o','--out_name', help='The name of the output directory to be made in out_dir. If unused, name will be inherited from input file names')    
     return parser.parse_args()
 
+
+def quality_args():
+    parser = argparse.ArgumentParser(description='Selected_tool : Quality. Executing this tool will assess a transcriptome assembly.')
+    parser.add_argument('tool_selector',help=argparse.SUPPRESS)
+    parser.add_argument('-test',help='Use this flag to test the pipeline.',action='store_true')
+    parser.add_argument('-u','--unpaired',help='A comma seperated list of unpaired fastq files.')
+    parser.add_argument('-1','--fastq1',help='A comma seperated list of fastq files. Each file should be paired with the same indexed file in fastq2.')
+    parser.add_argument('-2','--fastq2',help='A comma seperated list of fastq files. Each file should be paired with the same indexed file in fastq1.')
+    parser.add_argument('-cegma',help='Use this flag to run cegma as part of the annotation pipeline. Cegma is an old tool for assesing the quality of assemblies. Normal behavior of the pipeline is to use busco for assesing assemblies. Using this flag will run cegma in addition to Busco.',action='store_true')
+    parser.add_argument('--busco_ref',help='Set the reference that busco will use for analysis',default='metazoa')
+    parser.add_argument('--transrate_ref',help='A reference that transrate will use to evaluate the quality of your assembly.',default='')
+    parser.add_argument('-no_log',help='Pipeline will delete log files.',action='store_true')
+    parser.add_argument('-force',help='Use this flag to perform a fresh run of the pipeline. All steps will be executed regradless of what has already been performed.',action='store_true')
+    parser.add_argument('--cpu', help='Sets the process cap for execution. Default is 12. Use 0 to indicate no process cap should be used.',default=12,type=int)
+    parser.add_argument('--email',help='Pipeline will send emails informing you of runstate.')
+    parser.add_argument('--out_dir', help='Path to the ouput location. Defaults to assemblies directory inside pipeline',default=tf.PATH_ASSEMBLIES)
+    parser.add_argument('-o','--out_name', help='The name of the output directory to be made in out_dir. If unused, name will be inherited from input file names')
+    return parser.parse_args()
+
+
 def annotation_args():
     parser = argparse.ArgumentParser(description='Selected_tool : Annotation. Executing this tool will run asssembly quality assesment along with a series of tools designing to provide information about the assembled transcripts.')
     parser.add_argument('tool_selector',help=argparse.SUPPRESS)
+    parser.add_argument('-test',help='Use this flag to test the pipeline.',action='store_true')
     parser.add_argument('-a','--assembly',help='A fasta transcriptome assembly that will be used for computing expression levels.')
     parser.add_argument('-blastplus',action='store_true',help='Use the blast+ tool suite instead of diamond to align your transcripts to the references.')
     parser.add_argument('-uniref90',help='Use this flag to enable the uniref-90 blast runs as part of the annotation pipeline.',action='store_true')
@@ -103,6 +126,7 @@ def annotation_args():
 def expression_args():
     parser = argparse.ArgumentParser(description='Selected_tool : Expression. Executing this tool will run a series of differential expression analyses and sumarize the output.')
     parser.add_argument('tool_selector',help=argparse.SUPPRESS)
+    parser.add_argument('-test',help='Use this flag to test the pipeline.',action='store_true')
     parser.add_argument('-a','--assembly',help='A fasta transcriptome assembly that needs to be annotated.')
     parser.add_argument('--csv', help='A CSV file specifying fastq input, basenames for DE, and factors for DE. See online documentation for details on formating the CSV file.',default=None)
     parser.add_argument('--model', help='An optional list of comma seperated values used to run differential expression. This is particularly useful for refining Differential Expression runs as it allow you to use the same input CSV file and perform new comparisons.')
@@ -118,7 +142,10 @@ def expression_args():
 #####################____Argument_Testers____#####################
 def global_setup(args):
     if(not os.path.isdir(args.out_dir)):
-        raise Exception('\n\nERROR : invalid out_dir argument. Directory does not exist.')
+	try:
+             os.makedirs(args.out_dir)
+	except:
+            raise Exception('\n\nERROR : invalid out_dir argument. Path does not exist.')
     if(args.cpu<=0):
         args.cpu = float('inf')
     tf.PATH_ASSEMBLIES = args.out_dir
@@ -133,7 +160,7 @@ def global_setup(args):
             base = os.path.basename(args.fastq1[0])
             base = base if('.' not in base) else '.'.join(base.split('.')[:-1])
             base = base if('1' not in base) else '1'.join(base.split('1')[:-1])
-    if(args.tool_selector=='annotation' or args.tool_selector=='expression'):
+    if(args.tool_selector=='annotation' or args.tool_selector=='quality' or args.tool_selector=='expression'):
         base = os.path.basename(args.assembly)
         base = base if('.' not in base) else '.'.join(base.split('.')[:-1])
     tf.NAME_OUT_DIR = args.out_name if(args.out_name!=None) else base
@@ -154,21 +181,29 @@ def check_full_args(args):
 
 
 def check_assembly_args(args):
+    if(args.test):
+        args.out_name = args.out_name if(args.out_name!=None) else 'test_v2'
+        args.csv = tf.PATH_SCRIPTS+'/test_data/sample_info2.csv'
     args.subsample_size = 10**15 if(args.subsample_size<=0) else args.subsample_size*10**6
-    args.unpaired = [] if(args.unpaired==None) else args.unpaired.split(',')
-    args.fastq1 = [] if(args.fastq1==None) else args.fastq1.split(',')
-    args.fastq2 = [] if(args.fastq2==None) else args.fastq2.split(',')
-    if(args.unpaired==[] and args.fastq1==[] and args.fastq2==[]):
-        raise Exception('\n\nERROR : No input files specified. Please specify input using either --fastq1 and --fastq2 or --unpaired.')
-    for f in chain(args.fastq1,args.fastq2,args.unpaired):
-        if(not os.path.isfile(f)):
-            raise Exception('\n\nERROR : Unable to find file : '+f)
-    for f1,f2 in zip(args.fastq1,args.fastq2):
-        if(not fastq_pair_check(f1,f2)):
-            raise Exception('\n\nERROR : '+f1+' cant be paired with '+f2+'.')
-
+    if(args.csv):
+        handle_csv(args)
+    else:
+        args.unpaired = [] if(args.unpaired==None) else args.unpaired.split(',')
+        args.fastq1 = [] if(args.fastq1==None) else args.fastq1.split(',')
+        args.fastq2 = [] if(args.fastq2==None) else args.fastq2.split(',')
+        if(args.unpaired==[] and args.fastq1==[] and args.fastq2==[] and args.csv==[]):
+            raise Exception('\n\nERROR : No input files specified. Please specify input using either csv, or --fastq1 and --fastq2 or --unpaired.')
+        for f in chain(args.fastq1,args.fastq2,args.unpaired):
+            if(not os.path.isfile(f)):
+                raise Exception('\n\nERROR : Unable to find file : '+f)
+        for f1,f2 in zip(args.fastq1,args.fastq2):
+            if(not fastq_pair_check(f1,f2)):
+                raise Exception('\n\nERROR : '+f1+' cant be paired with '+f2+'.')
 
 def check_quality_args(args):
+    if(args.test):
+        args.out_name = args.out_name if(args.out_name!=None) else 'test_v2'
+        args.assembly = tf.PATH_SCRIPTS+'/test_data/test_v2.fasta'
     args.unpaired = [] if(args.unpaired==None) else args.unpaired.split(',')
     args.fastq1 = [] if(args.fastq1==None) else args.fastq1.split(',')
     args.fastq2 = [] if(args.fastq2==None) else args.fastq2.split(',')
@@ -185,6 +220,9 @@ def check_quality_args(args):
 
 
 def check_annotation_args(args):
+    if(args.test):
+        args.out_name = args.out_name if(args.out_name!=None) else 'test_v2'
+        args.assembly = tf.PATH_SCRIPTS+'/test_data/test_v2.fasta'
     if(args.assembly==None):
         raise Exception('\n\nERROR : No input assembly specified. Please specify an input assembly using --assembly.')
     if(not os.path.isfile(args.assembly)):
@@ -192,6 +230,10 @@ def check_annotation_args(args):
 
 
 def check_expression_args(args):
+    if(args.test):
+        args.out_name = args.out_name if(args.out_name!=None) else 'test_v2'
+        args.csv = tf.PATH_SCRIPTS+'/test_data/sample_info2.csv'
+        args.assembly = tf.PATH_SCRIPTS+'/test_data/test_v2.fasta'
     if(args.assembly==None):
         raise Exception('\n\nERROR : No input assembly specified. Please specify an input assembly using --assembly.')
     if(not os.path.isfile(args.assembly)):
@@ -226,8 +268,9 @@ def handle_csv(args):
     args.fastq1=[]
     args.fastq2=[]
     args.unpaired=[]
-    if(args.model==None):
-        args.model = ' '.join(head[3:])
+    if(args.tool_selector == 'expression' or args.tool_selector=='full'):
+        if(args.model==None):
+            args.model = ' '.join(head[3:])
     for line in f:
         fields = line.rstrip().split(',')
         if(fields[2]!=''):
@@ -260,15 +303,16 @@ def gen_sample_info(args):
 ######################___go_functions___######################
 def go_assembly(args, dep):
     return gen_assembly_supervisor(
-        args.fastq1, args.fastq2, args.unpaired, dep, args.busco_ref,
-        args.no_trim, args.rnaspades, args.no_rmdup, args.subsample_size,
-        args.cpu, args.cegma, args.subsample_seed, args.trinity_normalization,
-        args.truncate, args.transrate_ref, args.trimmomatic)
+        args.fastq1, args.fastq2, args.unpaired, dep,args.no_trim, 
+	args.rnaspades, args.no_rmdup, args.subsample_size,
+        args.cpu, args.subsample_seed, args.trinity_normalization,
+        args.truncate, args.trimmomatic)
 
 
 def go_quality(args, dep):
     return gen_quality_supervisor(
-        args.cpu, args.fastq1, args.fastq2, args.unpaired, dep)
+        args.assembly, args.fastq1, args.fastq2, args.unpaired, dep, args.busco_ref,
+	args.cpu, args.cegma,args.transrate_ref) 
 
 def go_annotation(args, dep):
     return gen_annotation_supervisor(
@@ -406,6 +450,6 @@ if(__name__ == '__main__'):
             args = quality_args()
             args.tool_selector = 'quality'
             run_quality(args)
-        raise Exception(err_str)
+        #raise Exception(err_str)
     else:
         raise Exception(err_str)
