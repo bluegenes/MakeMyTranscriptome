5b68c93fc91fd68787fe400d3f527a5aecabb6a0
bluegenes <ntpierce@gmail.com>
2015-12-10 09:33:21 -0800

    addStitle update

--- scripts/addStitleToBlastTab.py
+++ scripts/addStitleToBlastTab.py
@@ -12,21 +12,23 @@ blastD = {}
 with open(args.blast, 'r') as f:
     for line in f:
         line = line.rstrip().split('\t')
-        blastOrder.append(line[1])
-        blastD[line[1]] = line
-
+	blastOrder.append(line[1])
+	blastD[line[1]] = line
+    f.close()
 
 #potentially huge file --> don't want this in memory
 with open(args.db2Name, 'r') as f:
     for line in f:
         line = line.rstrip().split('\t')
-    hitInfo = blastD.get(line[0], None)
-    if hitInfo is not None:
-        hitInfo.extend(line[1:])
+	hitInfo = blastD.get(line[0], None)
+	if hitInfo is not None:
+	    hitInfo.extend(line[1:])
     f.close()
 
+#outExtendedTab = open(args.blast, 'w')
 for hit in blastOrder:
-    print('\t'.join(map(str,blastD[hit])) + '\n')
+    #outExtendedTab.write('\t'.join(map(str,blastD[hit])) + '\n')
+    sys.stdout.write('\t'.join(map(str,blastD[hit])) + '\n')
 
 
 
