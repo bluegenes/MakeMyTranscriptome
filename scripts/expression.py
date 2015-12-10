89c5ae3026a489c571e1eefa73c5a1509504bc78
bluegenes <ntpierce@gmail.com>
2015-12-10 09:43:59 -0800

    upd for salmon

--- scripts/expression.py
+++ scripts/expression.py
@@ -15,10 +15,11 @@ def gen_expression_supervisor(fastq1,fastq2,paired_names,unpaired,unpaired_names
     sam_sort_tasks = []
     intersect_tasks = []
     salmon_tasks = []
-    build_salmon = tf.build_salmon_task([])
+    build_salmon = tf.build_salmon_task(cpu,[])
     gene_trans_map = tf.gene_trans_map_task([])
-    salmon_gene_map = tf.salmon_gene_map_task([gene_trans_map_task])
-    salmon_tasks = [salmon_gene_map]
+    salmon_gene_map = tf.salmon_gene_map_task(gene_trans_map.targets[0],[gene_trans_map])
+    salmon_tasks = []
+    #salmon_tasks = [gene_trans_map,build_salmon,salmon_gene_map]
     for i in range(len(fastq1)):
 	bowtie_e = tf.bowtie2_task(fastq1[i],fastq2[i],paired_names[i]+'_express_bt2',0,int(cpu/2),[build_bowtie])
 	express = tf.express_task(bowtie_e.targets[0],paired_names[i],[bowtie_e])
@@ -47,11 +48,11 @@ def gen_expression_supervisor(fastq1,fastq2,paired_names,unpaired,unpaired_names
 	salmon_tasks.append(salmon)
     counts_to_table_express = tf.counts_to_table_task([t.targets[0] for t in express_tasks],'express_counts','--eXpress',express_tasks)
     counts_to_table_intersect = tf.counts_to_table_task([t.targets[0] for t in intersect_tasks],'bed_counts','',intersect_tasks)
-    counts_to_table_salmon = tf.counts_to_table_task([t.targets[0] for t in intersect_tasks],'salmon_counts','--salmon',salmon_tasks)
+    counts_to_table_salmon = tf.counts_to_table_task([t.targets[0] for t in salmon_tasks],'salmon_counts','--salmon',salmon_tasks)
     deseq2_express = tf.deseq2_task(counts_to_table_express.targets[0],sample_info,'express',model,[counts_to_table_express])
     deseq2_intersect = tf.deseq2_task(counts_to_table_intersect.targets[0],sample_info,'intersect',model,[counts_to_table_intersect])
     deseq2_salmon = tf.deseq2_task(counts_to_table_salmon.targets[0],sample_info,'salmon',model,[counts_to_table_salmon])
-    all_tasks = [fasta_to_bed,build_bowtie,counts_to_table_intersect,counts_to_table_express,counts_to_table_salmon, deseq2_intersect,deseq2_express, deseq2_salmon]
+    all_tasks = [fasta_to_bed,build_bowtie,build_salmon,gene_trans_map,salmon_gene_map,counts_to_table_intersect,counts_to_table_express,counts_to_table_salmon, deseq2_intersect,deseq2_express, deseq2_salmon]
     all_tasks.extend(bowtie_e_tasks+bowtie_i_tasks+express_tasks+sam_sort_tasks+intersect_tasks+salmon_tasks)
     return Supervisor(tasks=all_tasks,dependencies=dependency_set)
 
