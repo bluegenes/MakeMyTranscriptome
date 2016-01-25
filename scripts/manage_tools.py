62f27e9eebb8402fb14303b0cc514e0ce31a92bc
bluegenes <ntpierce@gmail.com>
2016-01-24 17:14:08 -0800

    testing out a fuller tool variables class

--- scripts/manage_tools.py
+++ scripts/manage_tools.py
@@ -5,7 +5,7 @@ install_trinity_task, install_trimmomatic_task, install_prinseq_task,
 install_transdecoder_task, install_hmmer_task, install_salmon_task,
 install_busco_task, install_transrate_task)
 import os
-from os.path import exists, join
+from os.path import exists, join, split
 import json
 import argparse
 import sys
@@ -18,95 +18,109 @@ if(sys.version[0] == '3'):
     from shutil import which as which
 else:
     from urllib import urlretrieve, ContentTooShortError
-    import py2_which as which
-
+    from  py2_which import which_python2 as which
 
 class tool_variables:
 
-    trinity_linux_url = 'https://github.com/trinityrnaseq/trinityrnaseq/archive/v2.1.1.tar.gz'
-    trinity_linux_target = join(PATH_TOOLS, 'trinityrnaseq-2.1.1')
-    trinity_exe = 'Trinity'
+    def __init__(self, name, url, target, executables, install_task):
+        self.name = name
+        self.url = url
+        self.targets = target
+        self.exe = executables
+        self.target = target
+	if install_task is not None:
+	    self.install = install_task(self.target, self.exe, [], False)
+	else:
+	    self.install = None
 
-    trimmomatic_url = 'http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.35.zip'
-    trimmomatic_trinity_target = join(which('Trinity'), 'trinity-plugins/Trimmomatic')
-    trimmomatic_url_target = join(PATH_TOOLS, 'Trimmomatic-0.35')
-    trimmomatic_exe = 'Trimmomatic-0.35.jar'
+    def add_exe(self, exe):
+        self.executables.append(exe)
 
-    prinseq_url = 'http://sourceforge.net/projects/prinseq/files/standalone/prinseq-lite-0.20.4.tar.gz'
-    prinseq_target = join(PATH_TOOLS, 'prinseq-lite-0.20.4')
-    prinseq_exe = 'prinseq-lite.pl'
+    def __call__(self):
+        return [self.name, self.url, self.target, self.executables, self.install]
 
-    transdecoder_url = 'https://github.com/TransDecoder/TransDecoder/archive/2.0.1.tar.gz'
-    transdecoder_target = join(PATH_TOOLS, 'TransDecoder-2.0.1')
-    transdecoder_exe1 = 'TransDecoder.Predict'
-    transdecoder_exe2 = 'TransDecoder.LongOrfs'
+########### links and targets ###########
+trinity_linux_url = 'https://github.com/trinityrnaseq/trinityrnaseq/archive/v2.1.1.tar.gz'
+trinity_linux_target = join(PATH_TOOLS, 'trinityrnaseq-2.1.1')
+trinity_exe = ['Trinity']
 
-    transrate_linux_url = 'https://bintray.com/artifact/download/blahah/generic/transrate-1.0.1-linux-x86_64.tar.gz'
-    transrate_linux_target = join(PATH_TOOLS, 'transrate-1.0.1-linux-x86_64')
-    transrate_exe = 'transrate'
+trimmomatic_url = 'http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.35.zip'
+trimmomatic_target = join(PATH_TOOLS, 'Trimmomatic-0.35')
+trimmomatic_exe = ['Trimmomatic-0.35.jar']
+#trimmomatic_trinity_target = join(split(which('Trinity'))[0], 'trinity-plugins/Trimmomatic')
 
-    busco_url = 'http://busco.ezlab.org/files/BUSCO_v1.1b1.tar.gz'
-    busco_target = join(PATH_TOOLS, 'BUSCO_v1.1b1')
-    busco_exe = 'BUSCO_v1.1b1.py'
+prinseq_url = 'http://sourceforge.net/projects/prinseq/files/standalone/prinseq-lite-0.20.4.tar.gz'
+prinseq_target = join(PATH_TOOLS, 'prinseq-lite-0.20.4')
+prinseq_exe = ['prinseq-lite.pl']
 
-    hmmer_linux_url = 'http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz'
-    hmmer_linux_target = join(PATH_TOOLS, 'hmmer-3.1b2-linux-intel-x86_64')
-    hmmer_exe1 = 'hmmscan' 
-    hmmer_exe2 = 'hmmpress'
+transdecoder_url = 'https://github.com/TransDecoder/TransDecoder/archive/2.0.1.tar.gz'
+transdecoder_target = join(PATH_TOOLS, 'TransDecoder-2.0.1')
+transdecoder_exe = ['TransDecoder.Predict','TransDecoder.LongOrfs']
 
-    diamond_linux_url = 'http://github.com/bbuchfink/diamond/releases/download/v0.7.10/diamond-linux64.tar.gz'
-    diamond_linux_target = join(PATH_TOOLS, '_diamond')
-    diamond_exe = 'diamond'
+transrate_linux_url = 'https://bintray.com/artifact/download/blahah/generic/transrate-1.0.1-linux-x86_64.tar.gz'
+transrate_linux_target = join(PATH_TOOLS, 'transrate-1.0.1-linux-x86_64')
+transrate_exe = ['transrate']
 
-    #salmon_linux_url = 'https://github.com/COMBINE-lab/salmon/releases/download/v0.5.1/SalmonBeta-0.5.1_DebianSqueeze.tar.gz'
-    #salmon_linux_target = join(PATH_TOOLS, 'SalmonBeta-0.5.1_DebianSqueeze')
-    salmon_linux_url = 'https://github.com/COMBINE-lab/salmon/releases/download/v0.6.0/SalmonBeta-0.6.0_DebianSqueeze.tar.gz'
-    salmon_linux_target = join(PATH_TOOLS, 'SalmonBeta-0.6.1_DebianSqueeze')
-    salmon_exe = 'salmon'
+busco_url = 'http://busco.ezlab.org/files/BUSCO_v1.1b1.tar.gz'
+busco_target = join(PATH_TOOLS, 'BUSCO_v1.1b1')
+busco_exe = ['BUSCO_v1.1b1.py']
 
-    # trinity_version_check = 'Trinity --version'
-    # prinseq_version_check = 'perl prinseq-lite.pl -version'
-    # transrateversion_check = 'transrate --version'
+hmmer_linux_url = 'http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz'
+hmmer_linux_target = join(PATH_TOOLS, 'hmmer-3.1b2-linux-intel-x86_64')
+hmmer_exe = ['hmmscan','hmmpress']
 
+diamond_linux_url = 'http://github.com/bbuchfink/diamond/releases/download/v0.7.10/diamond-linux64.tar.gz'
+diamond_target = join(PATH_TOOLS, '_diamond')
+diamond_exe = ['diamond']
+
+salmon_linux_url = 'https://github.com/COMBINE-lab/salmon/releases/download/v0.6.0/SalmonBeta-0.6.0_DebianSqueeze.tar.gz'
+salmon_linux_target = join(PATH_TOOLS, 'SalmonBeta-0.6.1_DebianSqueeze')
+salmon_exe = ['salmon']
+
+############# define the tools ##################
 tv = tool_variables
-tool_supervisor_log = '{0!s}/.tool_supervisor_log'.format(PATH_TOOLS)
+trinity_tool = tv('trinity', trinity_linux_url, trinity_linux_target, trinity_exe, install_trinity_task)
+trimmomatic_tool = tv('trimmomatic', trimmomatic_url, trimmomatic_target, trimmomatic_exe, install_trimmomatic_task)
+prinseq_tool = tv('prinseq', prinseq_url, prinseq_target, prinseq_exe, install_prinseq_task) 
+transdecoder_tool = tv('transdecoder', transdecoder_url, transdecoder_target, transdecoder_exe, install_transdecoder_task)
+transrate_tool = tv('transrate', transrate_linux_url, transrate_linux_target, transrate_exe, install_transrate_task)
+busco_tool = tv('busco', busco_url, busco_target, busco_exe, install_busco_task)
+hmmer_tool = tv('hmmer',hmmer_linux_url, hmmer_linux_target, hmmer_exe, install_hmmer_task)
+diamond_tool = tv('diamond', diamond_linux_url, diamond_target, diamond_exe, None)
+salmon_tool = tv('salmon', salmon_linux_url, salmon_linux_target, salmon_exe, install_salmon_task)
 
+tool_list = [trinity_tool, trimmomatic_tool, prinseq_tool, transdecoder_tool, transrate_tool,busco_tool, hmmer_tool, diamond_tool, salmon_tool]
+tvTools = {tool.name:tool for tool in tool_list}
+##################################################
+
+tool_supervisor_log = '{0!s}/.tool_supervisor_log'.format(PATH_TOOLS)
 
 def tools_join(p):
     return join(PATH_TOOLS, p)
 
+def tool_check(exe_list, allow_path=False):
+    external_tools = False
+    path_var = False
+    et_paths = [tools_join(x) for x in exe_list]
+    if all(exists(x) for x in et_paths):
+	external_tools = True
+    if allow_path:
+        if all(which(x) for x in exe_list):
+	    path_var = True
+    return any( [external_tools, path_var]) # just remove the 'any' and return both vals if want to be able to distinguish btwn our installs vs theirs...
 
 def check_tools(toolsD):
-    if(which(tv.trinity_exe) or exists(tools_join(tv.trinity_exe))):
-        toolsD['trinity'] = True
-    if(which(tv.prinseq_exe) or exists(tools_join(tv.prinseq_exe))):
-        toolsD['prinseq'] = True
-    if(which(tv.transrate_exe or exists(tools_join(tv.transrate_exe)))):
-        toolsD['transrate'] = True
-    if(which(tv.busco_exe) or exists(tools_join(tv.busco_exe))):
-        toolsD['busco'] = True
-    if(which(tv.salmon_exe) or exists(tools_join(tv.salmon_exe))):
-        toolsD['salmon'] = True
-    if(which(tv.diamond_exe) or exists(tools_join(tv.diamond_exe))):
-        toolsD['diamond'] = True
-    if(all([which(tv.transdecoder_exe1), which(tv.transdecoder_exe2)]) or
-       all([exists(tools_join(tv.transdecoder_exe1)), exists(tools_join(tv.transdecoder_exe2))])):
-        toolsD['transdecoder'] = True
-    if(all([which(tv.transdecoder_exe1), which(tv.transdecoder_exe2)]) or
-       all([exists(tools_join(tv.transdecoder_exe1)), exists(tools_join(tv.transdecoder_exe2))])):
-        toolsD['transdecoder'] = True
-    if(all([which(tv.hmmer_exe1), which(tv.hmmer_exe2)]) or
-       all([exists(tools_join(tv.hmmer_exe1)), exists(tools_join(tv.hmmer_exe2))])):
-        toolsD['hmmer'] = True
-    return toolsD
-
+    toolsToInstall = {}
+    for name, t in toolsD.items():
+        if not tool_check(t.exe):
+            toolsToInstall[name] = t
+    return toolsToInstall
 
 def check_dir_and_log():
     if(not os.path.isdir(PATH_TOOLS)):
         os.mkdir(PATH_TOOLS)
     if(not os.path.isfile(tools_join('tools_log'))):
         write_log({})
-
 
 def print_install_instructions(toolsDt):
     instructions = "some instructions will go here"
@@ -194,8 +208,11 @@ def main(install=False, toolList = [], tool_check=True, cpu=4):
     check_dir_and_log()
     toolsD = {}
     for tool in toolList:
-        toolsD[tool.lower()] = False
-    if tool_check:
+        t = tvTools[tool]
+        toolsD[t.name] = t
+	print(toolsD)
+	print(t.name, t.url, t.exe, t.target, t.install)
+    if tool_check: # --hard option --> don't want to check if installed
         toolsD = check_tools(toolsD) #return altered tools dictionary
     else:
         install = True
@@ -203,44 +220,49 @@ def main(install=False, toolList = [], tool_check=True, cpu=4):
     tasks = []
     partial_get = lambda a, b, c : get(log_table, a, b ,c, tool_check)
     if(install and platform.system().lower() == 'linux'):
-        if('trinity' in toolsD):  # if the tool is not in dictionary, we don't want to install
-            install_trinity = install_trinity_task( tv.trinity_linux_target, tv.trinity_exe, [], False)
-            partial_get('tar', tv.trinity_linux_url,  tv.trinity_linux_target)
-            tasks.append(install_trinity)
-        if('prinseq' in toolsD):
-            partial_get('tar', tv.prinseq_url, tv.prinseq_target)
-            install_prinseq = install_prinseq_task(tv.prinseq_target, tv.prinseq_exe, [], False)
-            tasks.append(install_prinseq)
-        if('transdecoder' in toolsD):
-            partial_get('tar', tv.transdecoder_url, tv.transdecoder_target)
-            install_transdecoder = install_transdecoder_task(tv.transdecoder_target, tv.transdecoder_exe1, tv.transdecoder_exe2, [], False)
-            tasks.append(install_transdecoder)
-        if('transrate' in toolsD):
-            partial_get('tar', tv.transrate_linux_url, tv.transrate_linux_target)
-            install_transrate = install_transrate_task(tv.transrate_linux_target, tv.transrate_exe, [], False)
-            tasks.append(install_transrate)
-        if('busco' in toolsD):
-            partial_get('tar', tv.busco_url, tv.busco_target)
-            install_busco = install_busco_task(tv.busco_target, tv.busco_exe, [], False)
-            tasks.append(install_busco)
-        if('diamond' in toolsD):
-            partial_get('tar', tv.diamond_linux_url, tv.diamond_linux_target)
-        if('hmmer' in toolsD):
-            partial_get('tar', tv.hmmer_linux_url, tv.hmmer_linux_target)
-            install_hmmer = install_hmmer_task(tv.hmmer_linux_target, tv.hmmer_exe1, tv.hmmer_exe2, [], False)
-            tasks.append(install_hmmer)
-        if('salmon' in toolsD):
-            partial_get('tar', tv.salmon_linux_url, tv.salmon_linux_target)
-            install_salmon = install_salmon_task(tv.salmon_linux_target, tv.salmon_exe, [], False)
-            tasks.append(install_salmon)
-#        if(not toolsD['trimmomatic']):
-#        download_external_tool(prinseq_url, trimmomatic_url_target)
-#           install_trimmomatic = install_trimmomatic_task(trimmomatic_url_target, trimmomatic_exe, [], False)
-#        tasks.append(install_trimmomatic)
+	for tool in toolD:
+	    #name = tool.name
+	    partial_get('tar', tool.url, tool.target)
+	    install_task = tool.install(tool.target, tool.exe, [], False)
+	    tasks.append(install_task)
+
+#if('trinity' in toolsD):  # if the tool is not in dictionary, we don't want to install
+#            install_trinity = install_trinity_task( tv.trinity_linux_target, tv.trinity_exe, [], False)
+#            partial_get('tar', tv.trinity_linux_url,  tv.trinity_linux_target)
+#            tasks.append(install_trinity)
+ #       if('prinseq' in toolsD):
+ #           partial_get('tar', tv.prinseq_url, tv.prinseq_target)
+ #           install_prinseq = install_prinseq_task(tv.prinseq_target, tv.prinseq_exe, [], False)
+ #           tasks.append(install_prinseq)
+ #       if('transdecoder' in toolsD):
+ #           partial_get('tar', tv.transdecoder_url, tv.transdecoder_target)
+#            install_transdecoder = install_transdecoder_task(tv.transdecoder_target, tv.transdecoder_exe1, tv.transdecoder_exe2, [], False)
+#            tasks.append(install_transdecoder)
+#        if('transrate' in toolsD):
+#            partial_get('tar', tv.transrate_linux_url, tv.transrate_linux_target)
+#            install_transrate = install_transrate_task(tv.transrate_linux_target, tv.transrate_exe, [], False)
+#            tasks.append(install_transrate)
+#        if('busco' in toolsD):
+#            partial_get('tar', tv.busco_url, tv.busco_target)
+#            install_busco = install_busco_task(tv.busco_target, tv.busco_exe, [], False)
+#            tasks.append(install_busco)
+#        if('diamond' in toolsD):
+#            partial_get('tar', tv.diamond_linux_url, tv.diamond_linux_target)
+#        if('hmmer' in toolsD):
+#            partial_get('tar', tv.hmmer_linux_url, tv.hmmer_linux_target)
+#            install_hmmer = install_hmmer_task(tv.hmmer_linux_target, tv.hmmer_exe1, tv.hmmer_exe2, [], False)
+#            tasks.append(install_hmmer)
+#        if('salmon' in toolsD):
+#            partial_get('tar', tv.salmon_linux_url, tv.salmon_linux_target)
+#            install_salmon = install_salmon_task(tv.salmon_linux_target, tv.salmon_exe, [], False)
+#            tasks.append(install_salmon)
+##        if('trimmomatic' in toolsD):
+#            partial_get('tar', tv.trimmomatic_url, tv.trimmomatic_target)
+#            install_trimmomatic = install_trimmomatic_task(tv.trimmomatic_target, tv.trimmomatic_exe, [], False)
+#            tasks.append(install_trimmomatic)
     else:
-        for tool, val in toolsD.items():
-            if(not val):
-                print_install_instructions(tool)
+        for tool in toolsD:
+            print_install_instructions(tool)
     run_tasks(tasks, cpu)
     write_log(log_table)
 
--- scripts/task_functions_v2.py
+++ scripts/task_functions_v2.py
@@ -97,8 +97,9 @@ def tool_path_check(name):
         print('WARNING : MMT has not installed '+name+'. MMT will use the version found in your path variable instead.')
         return name
     else:
-        raise EnvironmentError('MMT has not installed and was unable to detect '+name)
-
+        #raise EnvironmentError('MMT has not installed and was unable to detect '+name)
+	#try_to_install(name)
+        print('WARNING : MMT has not installed '+name+' and did not find this program in your path variable. Steps requiring '+name+'will not be performed.') 
 
 def build_dir_task(tasks):
     '''
@@ -799,64 +800,64 @@ def manage_tools_task(install, fresh, cpu_cap, tool_list, tasks, log_flag=True):
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err, cpu=cpu_cap)
 
 
-def install_trinity_task(trinity_target, trinity_exe, tasks, log_flag= True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,trinity_exe)]
-    cmd = 'cd {0!s}; make; ln -s {0!s}/{1!s} {2!s}/{1!s}'.format(trinity_target, trinity_exe, PATH_TOOLS)
+def install_trinity_task(trinity_target, trinity_exes, tasks, log_flag= True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,trinity_exes[0])]
+    cmd = 'cd {0!s}; make; ln -sf {0!s}/{1!s} {2!s}/{1!s}'.format(trinity_target, trinity_exes[0], PATH_TOOLS)
     name = 'install_trinity'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
 
-def install_trimmomatic_task(trimmomatic_target, trimmomatic_jar,  tasks, log_flag= True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS, trimmomatic_jar)]
-    cmd = 'cd {0!s}; ln -s {0!s}/{1!s} {2!s}/{1!s}'.format(trimmomatic_target, trimmomatic_jar, PATH_TOOLS)
+def install_trimmomatic_task(trimmomatic_target, trimmomatic_exes,  tasks, log_flag= True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS, trimmomatic_exes[0])]
+    cmd = 'cd {0!s}; ln -sf {0!s}/{1!s} {2!s}/{1!s}'.format(trimmomatic_target, trimmomatic_exes[0], PATH_TOOLS)
     name = 'install_trimmomatic'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
 
-def install_prinseq_task(prinseq_target, prinseq_exe,  tasks, log_flag= True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,prinseq_exe)]
-    cmd = 'cd {0!s}; ln -s {0!s}/{1!s} {2!s}/{1!s}'.format(prinseq_target, prinseq_exe, PATH_TOOLS)
+def install_prinseq_task(prinseq_target, prinseq_exes,  tasks, log_flag= True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,prinseq_exes[0])]
+    cmd = 'cd {0!s}; ln -sf {0!s}/{1!s} {2!s}/{1!s}'.format(prinseq_target, prinseq_exes[0], PATH_TOOLS)
     name = 'install_prinseq'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
 
-def install_transdecoder_task(transdecoder_target, transdecoder_exe1, transdecoder_exe2,  tasks, log_flag= True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,transdecoder_exe1), '{0!s}/{1!s}'.format(PATH_TOOLS,transdecoder_exe2)]
-    cmd = 'cd {0!s}; make; ln -s {0!s}/{1!s} {2!s}/{1!s}; ln -s {0!s}/{3!s} {2!s}/{3!s}'.format(transdecoder_target, transdecoder_exe1, PATH_TOOLS, transdecoder_exe2)
+def install_transdecoder_task(transdecoder_target, transdecoder_exes,  tasks, log_flag= True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,transdecoder_exes[0]), '{0!s}/{1!s}'.format(PATH_TOOLS,transdecoder_exes[1])]
+    cmd = 'cd {0!s}; make; ln -sf {0!s}/{1!s} {2!s}/{1!s}; ln -sf {0!s}/{3!s} {2!s}/{3!s}'.format(transdecoder_target, transdecoder_exes[0], PATH_TOOLS, transdecoder_exes[1])
     name = 'install_transdecoder'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
 
-def install_hmmer_task(hmmer_target, hmmer_exe1, hmmer_exe2, tasks, log_flag= True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS, hmmer_exe1),'{0!s}/{1!s}'.format(PATH_TOOLS, hmmer_exe2)]
-    cmd = 'cd {0!s}; ln -s {0!s}/binaries/{1!s} {2!s}/{1!s}; ln -s {0!s}/binaries/{3!s} {2!s}/{3!s}'.format(hmmer_target, hmmer_exe1, PATH_TOOLS, hmmer_exe2)
+def install_hmmer_task(hmmer_target, hmmer_exes, tasks, log_flag= True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS, hmmer_exes[0]),'{0!s}/{1!s}'.format(PATH_TOOLS, hmmer_exes[1])]
+    cmd = 'cd {0!s}; ln -sf {0!s}/binaries/{1!s} {2!s}/{1!s}; ln -sf {0!s}/binaries/{3!s} {2!s}/{3!s}'.format(hmmer_target, hmmer_exes[0], PATH_TOOLS, hmmer_exes[1])
     name = 'install_hmmer'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
-def install_salmon_task(salmon_target, salmon_exe, tasks, log_flag=True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,salmon_exe)]
-    cmd = 'cd {0!s}; ln -s {0!s}/bin/{1!s} {2!s}/{1!s}'.format(salmon_target, salmon_exe, PATH_TOOLS)
+def install_salmon_task(salmon_target, salmon_exes, tasks, log_flag=True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,salmon_exes[0])]
+    cmd = 'cd {0!s}; ln -sf {0!s}/bin/{1!s} {2!s}/{1!s}'.format(salmon_target, salmon_exes[0], PATH_TOOLS)
     #cmd = 'cd {0!s}; ln -s {0!s}/bin/{1!s} {2!s}/{1!s}; ln -s {0!s}/lib/* {2!s}/lib;'.format(salmon_target, salmon_exe, PATH_TOOLS)
     name = 'install_salmon'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
-def install_busco_task(busco_target, busco_exe,  tasks, log_flag= True):
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,busco_exe)]
-    cmd = 'cd {0!s}; ln -s {0!s}/{1!s} {2!s}/{1!s}'.format(busco_target, busco_exe, PATH_TOOLS)
+def install_busco_task(busco_target, busco_exes,  tasks, log_flag= True):
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,busco_exes[0])]
+    cmd = 'cd {0!s}; ln -sf {0!s}/{1!s} {2!s}/{1!s}'.format(busco_target, busco_exes[0], PATH_TOOLS)
     name = 'install_busco'
     out, err = GEN_LOGS(name) if(log_flag) else (None, None)
     return Task(command=cmd, dependencies=tasks, targets=trgs, name=name, stdout=out, stderr=err)
 
-def install_transrate_task(transrate_target, transrate_exe,  tasks, log_flag= True):
+def install_transrate_task(transrate_target, transrate_exes,  tasks, log_flag= True):
     #trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,transrate_exe), '{0!s}/bin/snap-aligner'.format(PATH_TOOLS), '{0!s}/lib/libtbb.so.2'.format(PATH_TOOLS)]
-    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,transrate_exe)]
-    cmd = 'cd {0!s}; ln -s {0!s}/{1!s} {2!s}/{1!s}'.format(transrate_target, transrate_exe, PATH_TOOLS)
+    trgs = ['{0!s}/{1!s}'.format(PATH_TOOLS,transrate_exes[0])]
+    cmd = 'cd {0!s}; ln -sf {0!s}/{1!s} {2!s}/{1!s}'.format(transrate_target, transrate_exes[0], PATH_TOOLS)
     #cmd = 'cd {0!s}; ln -s {0!s}/{1!s} {2!s}/{1!s}; ln -s {0!s}/bin/* {2!s}/bin; ln -s {0!s}/lib/* {2!s}/lib'.format(transrate_target, transrate_exe, PATH_TOOLS)
 #    cmd = 'os.environ["PATH"] += os.pathsep + {0!s}/bin; os.environ["LD_LIBRARY_PATH"] += os.pathsep + {0!s}/lib; ln -s {0!s}/{1!s} {2!s}/{1!s}'.format(transrate_target, transrate_exe, PATH_TOOLS)
     name = 'install_transrate'
