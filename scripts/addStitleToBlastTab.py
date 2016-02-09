eba99ef02dcc6584682c49e7b6a0524979c052e9
bluegenes <ntpierce@gmail.com>
2016-02-09 13:59:51 -0800

    better addStitle ordering

--- scripts/addStitleToBlastTab.py
+++ scripts/addStitleToBlastTab.py
@@ -7,29 +7,26 @@ parser.add_argument('-b','--blast', help='blast input file')
 
 args = parser.parse_args()
 
-blastOrder = []
-blastD = {}
+blastFile = []
+hitList = []
+hitDt = {}
 with open(args.blast, 'r') as f:
     for line in f:
         line = line.rstrip().split('\t')
-	blastOrder.append(line[1])
-	blastD[line[1]] = line
-    f.close()
+	blastFile.append(line)
+	hitList.append(line[1])
 
+hitSet = set(hitList)
 #potentially huge file --> don't want this in memory
 with open(args.db2Name, 'r') as f:
     for line in f:
         line = line.rstrip().split('\t')
-	hitInfo = blastD.get(line[0], None)
-	if hitInfo is not None:
-	    hitInfo.extend(line[1:])
+        if line[0] in hitSet:
+            hitDt[line[0]] = line[1:]
     f.close()
 
-#outExtendedTab = open(args.blast, 'w')
-for hit in blastOrder:
-    #outExtendedTab.write('\t'.join(map(str,blastD[hit])) + '\n')
-    sys.stdout.write('\t'.join(map(str,blastD[hit])) + '\n')
-
-
-
+for line in blastFile:
+    extraInfo = hitDt.get(line[1], [])
+    line = line + extraInfo
+    sys.stdout.write('\t'.join(map(str,line)) + '\n')
 
