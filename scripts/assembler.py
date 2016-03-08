6f51db4191e9281ee3419506114aae14eca47208
bluegenes <ntpierce@gmail.com>
2016-03-07 16:03:47 -0800

    fastqc task upd; allow gene_trans_map input

--- scripts/assembler.py
+++ scripts/assembler.py
@@ -4,6 +4,7 @@ import sys
 from tasks_v2 import Supervisor, Task
 import functions_general as fg
 import functions_assembler as fa
+import functions_annotater as fan
 import time
 
 
@@ -53,7 +54,7 @@ def gen_paired_trimmomatic_supervisor(out_dir,fq1, fq2, unpaired, dependency_set
 
 def gen_assembly_supervisor(out_dir, fastq1, fastq2, unpaired, dependency_set, no_trim=False, rnaSPAdes=False, rmdup=False, subset_size=50000000, cpu=12, subset_seed='I am a seed value', normalize_flag=False, truncate_opt=-1, trimmomatic_flag=False, path_assembly=fg.GEN_PATH_ASSEMBLY()):
     tasks = []
-    tasks.append(fa.fastqc_task(out_dir,fastq1+fastq2+unpaired, 'pre_trimming', []))
+    tasks.append(fa.fastqc_task(out_dir,fastq1+fastq2+unpaired,'pre_trimming',min(cpu,len(fastq1+fastq2+unpaired)), []))
     assembler_dependencies = []
     transrate_fastq1 = fastq1
     transrate_fastq2 = fastq2
@@ -69,7 +70,7 @@ def gen_assembly_supervisor(out_dir, fastq1, fastq2, unpaired, dependency_set, n
             transrate_fastq1 = fastq1
             transrate_fastq2 = fastq2
             tasks.append(paired_sup)
-            tasks.append(fa.fastqc_task(out_dir, fastq1+fastq2, 'post_trimming_paired', [paired_sup]))
+            tasks.append(fa.fastqc_task(out_dir, fastq1+fastq2, 'post_trimming_paired',int(round(float(cpu)/2)),[paired_sup]))
         subset_dependencies = [paired_sup] if(not no_trim) else []
         subset = fa.subset_task(out_dir, fastq1, fastq2, 'final_reads', subset_size, subset_seed, subset_dependencies)
         fastq1 = [subset.targets[0]]
@@ -77,7 +78,7 @@ def gen_assembly_supervisor(out_dir, fastq1, fastq2, unpaired, dependency_set, n
         tasks.append(subset)
         assembler_dependencies = [subset]
         if subset_size < 10**15: # some subsetting may have occurred
-	    late_fastqc = fa.fastqc_task(out_dir, subset.targets, 'final_reads_paired', [subset])
+	    late_fastqc = fa.fastqc_task(out_dir, subset.targets, 'final_reads_paired',int(round(float(cpu)/2)),[subset])
             tasks.append(late_fastqc)
         if(truncate_opt >= 0):
             truncate = fa.truncate_task(out_dir, fastq1[0], fastq2[0], truncate_opt, [subset])
@@ -94,14 +95,14 @@ def gen_assembly_supervisor(out_dir, fastq1, fastq2, unpaired, dependency_set, n
             unpaired = unpaired_sup.targets
             transrate_unpaired = unpaired
             tasks.append(unpaired_sup)
-            tasks.append(fa.fastqc_task(out_dir,unpaired, 'post_trimming_unpaired', [unpaired_sup]))
+            tasks.append(fa.fastqc_task(out_dir,unpaired, 'post_trimming_unpaired',int(round(float(cpu)/2)),[unpaired_sup]))
             assembler_dependencies.append(unpaired_sup)
     if(rnaSPAdes):
         rnaspades = fa.rnaspades_task(path_assembly, out_dir,fastq1, fastq2, unpaired, cpu, assembler_dependencies)
         tasks.append(rnaspades)
     else:
         trinity = fa.trinity_task(path_assembly, out_dir, fastq1, fastq2, unpaired, cpu, int(cpu/2), 120, 120, normalize_flag, assembler_dependencies)
-        tasks.append(trinity)
+	tasks.append(trinity)
     assembler_main_task = tasks[-1]
     return Supervisor(tasks=tasks)
 
