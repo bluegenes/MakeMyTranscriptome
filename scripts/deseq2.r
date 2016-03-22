3583cab2e0ae3fda815a132d72b680c8b6eb9990
bluegenes <ntpierce@gmail.com>
2016-03-22 14:30:43 -0700

    added scripts_dir into deseq2 params

--- scripts/deseq2.r
+++ scripts/deseq2.r
@@ -8,23 +8,25 @@
 
 # get args from bash
 args <- commandArgs(trailingOnly = TRUE)
-countTableFile = args[2]
-treatmentInfoFile = args[3]
-baseDir = args[4]
+scriptsDir = args[2]
+countTableFile = args[3]
+treatmentInfoFile = args[4]
+baseDir = args[5]
 
 ## read in metadata ## 
 treatmentInfo <- read.table(treatmentInfoFile, header=TRUE, row.names = 1, stringsAsFactors =TRUE)
 ## set model from command line OR from order of factors in the treatmentInfoFile
-if (length(args) >= 6){ 
-    modelParam = paste("~",paste((args[6:length(args)]), collapse = "+")) 
-    baseName = paste(args[5],paste((args[6:length(args)]), collapse = "_"),sep='_')
+if (length(args) >= 7){ 
+    modelParam = paste("~",paste((args[7:length(args)]), collapse = "+")) 
+    baseName = paste(args[6],paste((args[7:length(args)]), collapse = "_"),sep='_')
 }else{
     modelParam = paste("~",paste(rev(colnames(treatmentInfo)), collapse = "+")) 
-    baseName = paste(args[5],paste(rev(colnames(treatmentInfo)), collapse = "_"),sep='_') # reverse order, so 1st/most impt = last in model
+    baseName = paste(args[6],paste(rev(colnames(treatmentInfo)), collapse = "_"),sep='_') # reverse order, so 1st/most impt = last in model
     }  
 
 # set up the R environment
-source('autoDESeq2functions.r') #wrapper functions necessary for this script to run
+
+source(paste(scriptsDir,'autoDESeq2functions.r', sep = "/")) #wrapper functions necessary for this script to run
 setwd(baseDir)# set output directory 
 
 ## read in count data ##  
