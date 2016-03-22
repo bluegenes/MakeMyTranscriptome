8f1366a0c2349d3bc045e903cb54a82f66f39170
bluegenes <ntpierce@gmail.com>
2016-03-22 14:51:17 -0700

    minor upd filter

--- scripts/filter_super.py
+++ scripts/filter_super.py
@@ -11,8 +11,9 @@ def gen_filter_supervisor(main_path_assembly,main_assembly_name,out_dir, transra
     tasks = [] 
     filter_full = fg.filter_task(main_path_assembly,main_assembly_name,out_dir,[transrate_task.targets[2]],tpm_threshold,2,[transrate_task])
     tasks.append(filter_full)
-    filter_good = fg.filter_task(transrate_task.targets[1],'good.'+ main_assembly_name, out_dir, [transrate_task.targets[2]], tpm_threshold, 2,[transrate_task])
-    tasks.append(filter_good)
+    if transrate_task is not None:
+        filter_good = fg.filter_task(transrate_task.targets[1],'good.'+ main_assembly_name, out_dir, [transrate_task.targets[2]], tpm_threshold, 2,[transrate_task])
+        tasks.append(filter_good)
     return Supervisor(tasks=tasks,dependencies=dependency_set)
 
 if(__name__ == '__main__'):
