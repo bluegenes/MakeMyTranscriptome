00e5c9b9bcff917d7680f21bd5309f6183feec51
bluegenes <ntpierce@gmail.com>
2016-05-13 16:11:41 -0700

    add sudo_setup.sh script

---
+++ sudo_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+
+sudo apt-get update
+sudo apt-get install -y screen git curl gcc make g++ python-dev unzip default-jre pkg-config libncurses5-dev r-base-core r-cran-gplots python-matplotlib python-pip python-virtualenv python-pandas sysstat fastqc trimmomatic bowtie samtools blast2 cmake libboost-all-dev liblzma-dev r-bioc-edgeR hmmer ncbi-blast+-legacy emboss  libbz2-1.0 libbz2-dev openjdk-6-jre-headless zlib1g-dev libxml2 libxml2-dev gfortran-4.6
+
+sudo pip install fadapa
+
